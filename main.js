e4b90ced8d2ceb7e1ff817603752bbc97c7d6ac4
Rashika Tasnim <35694006+rashika-tasnim@users.noreply.github.com>
2019-03-30 13:06:59 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+// Get the video
+var video = document.getElementById("myVideo");
+
+// Get the button
+var btn = document.getElementById("myBtn");
+
+// Pause and play the video, and change the button text
+function myFunction() {
+  if (video.paused) {
+    video.play();
+    btn.innerHTML = "Pause";
+  } else {
+    video.pause();
+    btn.innerHTML = "Play";
+  }
+}
